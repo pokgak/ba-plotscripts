323b5a714702626440e6fff95e87fa4c86e12373
M Aiman Ismail <MuhammadAimanBin.Ismail@haw-hamburg.de>
2020-11-02 11:49:44 +0100

    exclude first 10 samples to reduce outliers

--- plot_jitter_combined.py
+++ plot_jitter_combined.py
@@ -18,6 +18,7 @@ def plot_jitters(basedir, boards):
     data = {
         "bg_timer_count": [],
         "main_timer_interval": [],
+        "i": [],
         "sleep_duration": [],
         "timer_version": [],
         "board": [],
@@ -48,11 +49,12 @@ def plot_jitters(basedir, boards):
             )
             # HACK: we repeat measurement for 51 times and
             # discard the first measurement as it is most likely 0
-            traces = traces[1:]
+            # traces = traces[1:]
             # Philip record in seconds, convert to milliseconds
             traces = [v * 1000000 for v in traces]
 
             data["sleep_duration"].extend(traces)
+            data['i'].extend(range(len(traces)))
             data["bg_timer_count"].extend([bg_timer_count] * len(traces))
             data["main_timer_interval"].extend([main_timer_interval] * len(traces))
             data["timer_version"].extend([version] * len(traces))
@@ -61,22 +63,35 @@ def plot_jitters(basedir, boards):
 
 
 df = plot_jitters(basedir, boards)
+
+# the earlier sample points might be too small to trigger
+df.drop(df[(df["i"] <= 10)].index, inplace=True)
+
 df["diff_actual_target_duration"] = df["sleep_duration"] - (df["main_timer_interval"])
 
-fig = px.violin(
+fig = px.box(
     df,
     x="bg_timer_count",
     y="diff_actual_target_duration",
     color="timer_version",
     facet_col="board",
-    facet_col_wrap=4,
+    facet_col_wrap=2,
     facet_col_spacing=0.06,
+    # points="all",
+    hover_data=['i'],
 )
 
 fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
 fig.update_yaxes(matches=None, showticklabels=True)
-fig.update_xaxes(tick0=0, dtick=5)
+fig.update_xaxes(tick0=0, dtick=5, showticklabels=True)
 
+# legend
+fig.update_layout(legend=dict(
+    title="Timer Version",
+    orientation="h",
+    x=0,
+    y=1.1,
+))
 # hide original title
 fig.update_yaxes(title_text="")
 fig.update_xaxes(title_text="")
@@ -99,5 +114,6 @@ fig.add_annotation(
     showarrow=False,
 )
 
-# fig.write_html("/tmp/jitter_combined.html", include_plotlyjs="cdn")
+fig.write_html("/tmp/jitter_combined.html", include_plotlyjs="cdn")
 fig.write_image(f"{outdir}/jitter_combined.pdf", height=900, width=900)
+print(f"{outdir}/jitter_combined.pdf")
