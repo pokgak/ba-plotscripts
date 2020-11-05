208f6d09cbc9d3d064ef36cf78d01f33e637adab
M Aiman Ismail <MuhammadAimanBin.Ismail@haw-hamburg.de>
2020-11-05 12:02:02 +0100

    fixup! jitter: done parsing new output

--- plot_jitter_combined.py
+++ plot_jitter_combined.py
@@ -1,6 +1,9 @@
+# %%
 import os
+import re
 import argparse
 import itertools
+from typing import Pattern
 
 import xml.etree.ElementTree as ET
 import plotly.graph_objects as go
@@ -8,6 +11,8 @@ import plotly.express as px
 import pandas as pd
 
 from ast import literal_eval
+
+# %%
 
 # outdir = "/home/pokgak/git/ba-plotscripts/docs/timer_benchmarks/result"
 # basedir = "/home/pokgak/git/ba-plotscripts/docs/timer_benchmarks/data"
@@ -17,52 +22,75 @@ basedir = "/home/pokgak/git/RobotFW-tests/build/robot"
 boards = os.listdir(basedir)
 
 
-def get_value(element, property):
-    return element.find("properties/property[@name='{}']".format(property)).get("value")
+def get_value(property):
+    return literal_eval(property.get("value"))
+
+
+def get_result_type(property):
+    return property.get("name").split("-")[0]
+
+
+def get_timer_count(property):
+    return property.get("name").split("-")[1]
 
 
 def parse_result(basedir, boards):
     data = {
         "timer_count": [],
         "timer_interval": [],
-        # "i": [],
-        "dut_start_time": [],
-        "dut_wakeup_time": [],
-        "hil_start_time": [],
-        "hil_wakeup_time": [],
+        "i": [],
+        "start_time": [],
+        "wakeup_time": [],
+        "result_type": [],
         "timer_version": [],
         "board": [],
     }
 
+    entries = list()
     for version, board in itertools.product(["xtimer", "ztimer"], boards):
-        inputfile = f"{basedir}/{board}/tests_{version}_benchmarks/xunit.xml"
-        for testcase in ET.parse(inputfile).findall(
-            f"testcase[@classname='tests_{version}_benchmarks.Sleep Jitter']"
-        ):
-            timer_count = int(get_value(testcase, "timer-count"))
-            timer_interval = int(get_value(testcase, "timer-interval"))
+        inputfile = "{:s}/{:s}/tests_{:s}_benchmarks/xunit.xml".format(
+            basedir, board, version
+        )
 
-            dut_start_time = literal_eval(get_value(testcase, "dut-start-time"))
-            dut_wakeup_time = literal_eval(get_value(testcase, "dut-wakeup-time"))
+        testcase = ET.parse(inputfile).find(
+            "testcase[@classname='tests_{:s}_benchmarks.Sleep Jitter']".format(version)
+        )
 
-            hil_wakeup_time = literal_eval(get_value(testcase, "hil-wakeup-time"))
-            hil_start_time = literal_eval(get_value(testcase, "hil-start-time"))
+        if testcase is None:
+            continue
 
-            # Philip record in seconds, convert to microseconds
-            hil_start_time = hil_start_time * 1000000
-            hil_wakeup_time = [v * 1000000 for v in hil_wakeup_time]
+        timer_interval = testcase.find(".//property[@name='timer-interval']")
+        if timer_interval is None:
+            raise RuntimeError("timer_interval not found")
 
-            data["hil_start_time"].extend([hil_start_time] * len(hil_wakeup_time))
-            data["dut_start_time"].extend([dut_start_time] * len(dut_wakeup_time))
+        start_times = [
+            prop
+            for prop in testcase.findall(".//property")
+            if prop.get("name").endswith("start-time")
+        ]
+        wakeup_times = [
+            prop
+            for prop in testcase.findall(".//property")
+            if prop.get("name").endswith("wakeup-time")
+        ]
 
-            data["hil_wakeup_time"].extend(hil_wakeup_time)
-            data["dut_wakeup_time"].extend(dut_wakeup_time)
+        for start, wakeup in zip(start_times, wakeup_times):
+            if get_result_type(start) != get_result_type(wakeup):
+                raise RuntimeError("Result type does not match")
 
-            data["timer_count"].extend([timer_count] * len(hil_wakeup_time))
-            data["timer_interval"].extend([timer_interval] * len(hil_wakeup_time))
+            result_type = get_result_type(start)
+            timer_count = get_timer_count(start)
+            w = get_value(wakeup)
+            w = w if result_type == "dut" else [t * 1000000 for t in w]
+            data["i"].extend(range(len(w)))
+            data["wakeup_time"].extend(w)
+            data["start_time"].extend([get_value(start)] * len(w))
+            data["result_type"].extend([result_type] * len(w))
+            data["timer_count"].extend([timer_count] * len(w))
 
-            data["timer_version"].extend([version] * len(hil_wakeup_time))
-            data["board"].extend([board] * len(hil_wakeup_time))
+        data["timer_interval"] = [get_value(timer_interval)] * len(data["wakeup_time"])
+        data["timer_version"] = [version] * len(data["wakeup_time"])
+        data["board"] = [board] * len(data["wakeup_time"])
 
     for k, v in data.items():
         print(k, len(v))
@@ -71,12 +99,32 @@ def parse_result(basedir, boards):
 
 df = parse_result(basedir, boards)
 
+df["diff_from_start"] = df["wakeup_time"] - df["start_time"]
+df["diff_from_target"] = df["diff_from_start"] % df["timer_interval"]
+
+# %%
+fig = px.box(
+    df,
+    x="timer_count",
+    y="diff_from_target",
+    color="timer_version",
+    hover_data=["i", "diff_from_target", "diff_from_start"],
+    # facet_col="timer_count",
+    facet_row="result_type",
+    points="all",
+)
+
+fig.update_yaxes(matches=None, showticklabels=True, title="Difference from target [us]")
+fig.update_layout(
+    legend_title="Timer Version",
+)
+
+fig
+
 # %%
 
 # the earlier sample points might be too small to trigger
 # df.drop(df[(df["i"] == 0)].index, inplace=True)
-
-df["diff_actual_target_duration"] = df["sleep_duration"] - (df["main_timer_interval"])
 
 fig = px.box(
     df,
