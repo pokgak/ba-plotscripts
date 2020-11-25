3d84a3c75b01ddb0754a4123d64a5e8fff11a618
M Aiman Ismail <MuhammadAimanBin.Ismail@haw-hamburg.de>
2020-11-25 15:14:30 +0100

    update repeat stats plot

--- plot_repeat_stats.py
+++ plot_repeat_stats.py
@@ -313,7 +313,7 @@ def accuracy_dist():
 
 
 if __name__ == "__main__":
-    # timer_now_dist()
-    # set_remove_dist()
-    # accuracy_dist()
+    timer_now_dist()
+    set_remove_dist()
+    accuracy_dist()
     jitter_dist()
